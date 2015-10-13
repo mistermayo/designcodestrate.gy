4fcca1096400a3d90a6543379ab3a1dd8513787a
Tucker <tuckerjamesmayo@gmail.com>
2015-10-12 22:01:27 -0700

    add formspree ajax

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-              url: "//formspree.io/charlie@designcodestrate.gy",
+              url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
---
+++ js/form_spree.js
@@ -0,0 +1,6 @@
+$.ajax({
+    url: "//formspree.io/charlie@designcodestrate.gy",
+    method: "POST",
+    data: {message: "hello!"},
+    dataType: "json"
+});
